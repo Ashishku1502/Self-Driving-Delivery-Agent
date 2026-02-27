53908abf0c458d1f1a8ab918000b06cf60347de2
ashishku1502@gmail.com <ashishku1502@gmail.com>
2026-02-28 00:20:55 +0530

    Merge driVLMe as plain files and add full codebase

---
+++ driVLMe/drivlme/mm_utils.py
@@ -0,0 +1,29 @@
+import torch
+from drivlme.constants import IMAGE_TOKEN_INDEX, DEFAULT_IMAGE_TOKEN
+
+def tokenizer_image_token(prompt, tokenizer, image_token_index=IMAGE_TOKEN_INDEX, return_tensors=None):
+    prompt_chunks = [tokenizer(chunk).input_ids for chunk in prompt.split(DEFAULT_IMAGE_TOKEN)]
+
+    def insert_separator(X, sep):
+        return [ele for sublist in zip(X, [sep]*len(X)) for ele in sublist][:-1]
+
+    input_ids = []
+    offset = 0
+    if len(prompt_chunks) > 0 and len(prompt_chunks[0]) > 0 and prompt_chunks[0][0] == tokenizer.bos_token_id:
+        offset = 1
+        input_ids.append(prompt_chunks[0][0])
+
+    for x in insert_separator(prompt_chunks, [image_token_index] * (offset + 1)):
+        input_ids.extend(x[offset:])
+
+    if return_tensors is not None:
+        if return_tensors == 'pt':
+            return torch.tensor(input_ids, dtype=torch.long)
+        raise ValueError(f'Unsupported return_tensors: {return_tensors}')
+
+    return input_ids
+
+def get_model_name_from_path(model_path):
+    model_path = model_path.strip("/")
+    model_name = model_path.split("/")[-1]
+    return model_name
